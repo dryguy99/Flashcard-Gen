dcf006724c50b0a1749448aaa300134ad16d25c2
Timothy Dry <timothy.dry@verizon.net>
2017-04-03 15:35:56 -0400

    updated basic function

--- flashcard.js
+++ flashcard.js
@@ -1,10 +1,21 @@
+var fs = require("fs");
 
 
 function basicCard(front, back){
+
   if (this instanceof basicCard){
+  	this.file = "basic-card.txt";
     this.front = front;
     this.back = back;
   } else {
     return new basicCard(front, back);  
   }
+  fs.appendFile(this.file, JSON.stringify({
+			front: this.front,
+			back: this.back
+		}) + "\n", function(error){
+			if(error){ 	console.log("error appending");
+						console.log(error);}
+		});
 }// end basicCard function
+module.exports = basicCard(front, back);
