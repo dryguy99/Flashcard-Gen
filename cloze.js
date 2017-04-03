72677fc7dbf6585a2383bd1393debb3d3b8574a6
Timothy Dry <timothy.dry@verizon.net>
2017-04-03 15:39:31 -0400

    added cloze card function

---
+++ cloze.js
@@ -0,0 +1,22 @@
+
+var fs = require("fs");
+
+
+function clozeCard(front, back){
+
+  if (this instanceof clozeCard){
+  	this.file = "cloze-card.txt";
+    this.front = front;
+    this.back = back;
+  } else {
+    return new clozeCard(front, back);  
+  }
+  fs.appendFile(this.file, JSON.stringify({
+			front: this.front,
+			back: this.back
+		}) + "\n", function(error){
+			if(error){ 	console.log("error appending");
+						console.log(error);}
+		});
+}// end basicCard function
+module.exports = basicCard(front, back);
