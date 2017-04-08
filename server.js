8a7315ccb29b68af653b5438595c4e9335ae04a4
Timothy Dry <timothy.dry@verizon.net>
2017-04-08 14:22:34 -0400

    updated code

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    console.log("allowCrossDomain called");
 
     // intercept OPTIONS method
     if ('OPTIONS' == req.method) {
@@ -116,7 +117,7 @@ router.get('/', function(req, res) {
 router.route('/basic')
 
 // create a basic card (accessed at POST http://localhost:8080/api/basic)
-    .post(function(req, res) {
+    .post(allowCrossDomain, function(req, res) {
     	var front = req.query.front;
         var back = req.query.back;
     	function postbasicMysql(front, back){
